ccc8583257f184bb9911bfdd36a142c3e85f9e43
mariohotline <mariohotline14@gmail.com>
2024-09-20 02:03:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ pipeBottom.src = 'columndown.png'; // Your bottom pipe image
 // General settings
 let gamePlaying = false;
 let gravity = 0.5;
-let speed = 6.2;
+let speed = 6.2; 
 let birdSize = [100, 100]; // Adjust to your bird image size
 let jump = -11.5;
 let cTenth = canvas.width / 10; // Horizontal position of the bird
